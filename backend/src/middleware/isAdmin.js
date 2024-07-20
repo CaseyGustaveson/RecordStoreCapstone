6a479d1ad02254659b5acef35f2d2d319044bd4a
CaseyGustaveson <161667272+CaseyGustaveson@users.noreply.github.com>
2024-07-20 14:11:52 -0500

    safety commit

--- backend/src/middleware/isAdmin.js
+++ backend/src/middleware/isAdmin.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const isAdmin = (req, res, next) => {
+=======
+export const isAdmin = (req, res, next) => {
+>>>>>>> 894ada7 (recovery commit)
   if (req.user && req.user.role === 'ADMIN') {
     next();
   } else {
