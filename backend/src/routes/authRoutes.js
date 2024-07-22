c15b1036b81088fa3981f5a3c8d007c64ebef03a
CaseyGustaveson <161667272+CaseyGustaveson@users.noreply.github.com>
2024-07-21 19:59:53 -0500

    rebase

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,12 @@
 import express from 'express';
+<<<<<<< HEAD
 const router = express.Router();
 import { register, login, logout } from '../controllers/authController.js'; 
+=======
+import { register, login, logout } from '../controllers/authController.js';
+
+const router = express.Router();
+>>>>>>> b74b19b (do-over)
 
 // Middleware to parse JSON bodies
 router.use(express.json());
@@ -10,4 +16,8 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 
+<<<<<<< HEAD
 export default router;
+=======
+export default router;
+>>>>>>> b74b19b (do-over)
--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -1,14 +1,27 @@
 import express from 'express';
+<<<<<<< HEAD
 const router = express.Router();
 import * as categoryController from '../controllers/categoryController.js';
 import { authenticateToken, isAdmin } from '../middleware/authMiddleware.js';
 
 // Middleware to parse JSON bodies
+=======
+import * as categoryController from '../controllers/categoryController.js';
+import { authenticateToken, isAdmin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+// Middleware to parse JSON request body
+>>>>>>> b74b19b (do-over)
 router.use(express.json());
 
 // Routes
 router.get('/', categoryController.getCategories);
+<<<<<<< HEAD
 router.get('/:id', categoryController.getCategoryById);
+=======
+router.get('/:id', categoryController.getCategory);
+>>>>>>> b74b19b (do-over)
 router.post('/', authenticateToken, isAdmin, categoryController.createCategory);
 router.put('/:id', authenticateToken, isAdmin, categoryController.updateCategory);
 router.delete('/:id', authenticateToken, isAdmin, categoryController.deleteCategory);
