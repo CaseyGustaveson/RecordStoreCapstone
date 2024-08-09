574a9b7ab07e1c8bd4368e9b264596192a1c8d9c
Casey <gustcase@isu.edu>
2024-08-09 11:00:37 -0500

    idk

--- backend/app.js
+++ backend/app.js
@@ -6,17 +6,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Admin Dashboard route
+
 app.get('/admin', (req, res) => {
     res.send('Welcome to the admin dashboard!');
 });
 
-// Default route
 app.get('/', (req, res) => {
     res.send('Hello friend!');
 });
 
-// Error handling middleware
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
