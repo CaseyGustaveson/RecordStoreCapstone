696cd54da411fad92cb6c75c62368905ce5d2e44
CaseyGustaveson <161667272+CaseyGustaveson@users.noreply.github.com>
2024-09-13 06:21:27 -0500

    Update AdminProducts.jsx

--- frontend/src/pages/Admin/AdminProducts.jsx
+++ frontend/src/pages/Admin/AdminProducts.jsx
@@ -16,10 +16,9 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import ProductCard from "../../components/ProductCard";
 
-const PRODUCTS_API_URL = import.meta.env.VITE_PRODUCTS_API_URL;
-const CATEGORY_API_URL = import.meta.env.VITE_CATEGORY_API_URL;
+
+const API_URL= import.meta.env.VITE_API_URL;
 
 const AdminProducts = () => {
   const navigate = useNavigate();
@@ -55,8 +54,8 @@ const AdminProducts = () => {
     const fetchInitialData = async () => {
       try {
         const [productsResponse, categoriesResponse] = await Promise.all([
-          axios.get(PRODUCTS_API_URL, { headers: { Authorization: `Bearer ${token}` } }),
-          axios.get(CATEGORY_API_URL, {
+          axios.get(`${API_URL}/api/products`, { headers: { Authorization: `Bearer ${token}` } }),
+          axios.get(`${API_URL}/api/categories`, {
             headers: { Authorization: `Bearer ${token}` },
           }),
         ]);
@@ -90,9 +89,9 @@ const AdminProducts = () => {
 
   const addProduct = async () => {
     try {
-      console.log("Adding product with data:", newProduct); // Log the product data
+      console.log("Adding product with data:", newProduct); 
       const response = await axios.post(
-        PRODUCTS_API_URL,
+        `${API_URL}/api/products/`,
         {
           ...newProduct,
           categoryId: newProduct.categoryId,
@@ -120,7 +119,7 @@ const AdminProducts = () => {
   const editProduct = async (id) => {
     try {
       const response = await axios.put(
-        `${PRODUCTS_API_URL}/${id}`,
+        `${API_URL}/api/products/${id}`,
         {
           ...newProduct,
           price: newProduct.price.toString(), // Ensure price is a string
@@ -157,7 +156,7 @@ const AdminProducts = () => {
 
   const deleteProduct = async (id) => {
     try {
-      await axios.delete(`${PRODUCTS_API_URL}/${id}`, {
+      await axios.delete(`${API_URL}/api/products/${id}`, {
         headers: { Authorization: `Bearer ${token}` },
       });
       setProducts(products.filter((product) => product.id !== id));
